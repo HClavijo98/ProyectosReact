7a47ada05072ddfc6e81da364eb2d40665d352a7
zukolinoProPro <22hclavijo@ibadia.cat>
2024-10-18 18:03:56 +0200

    buscadorPeliculas(final)

--- BuscadorPeliculas/src/App.jsx
+++ BuscadorPeliculas/src/App.jsx
@@ -2,21 +2,35 @@ import './App.css'
 import { Movies } from './components/Movies.jsx'
 import { useMovies } from './hooks/useMovies.js'
 import { useSearch } from './hooks/useSearch.js'
+import { useState, useCallback } from 'react'
+import debounce from 'just-debounce-it'
 
 // https://www.omdbapi.com/
 // http://www.omdbapi.com/?apikey=[yourkey]&
 
 export function App () {
+  const [sort, setSort] = useState(false)
   const { search, setSearch, error } = useSearch()
-  const { movies, getMovies, loading } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies(search)
+    }, 500)
+    , [getMovies])
 
   function handleSubmit (event) {
     event.preventDefault()
-    getMovies()
+    getMovies(search)
+  }
+
+  function handleSort () {
+    setSort(!sort)
   }
 
   function handleChange (event) {
-    setSearch(event.target.value)
+    const newSearch = event.target.value
+    setSearch(newSearch)
+    debouncedGetMovies(newSearch)
   }
 
   return (
@@ -31,6 +45,7 @@ export function App () {
               borderColor: error ? 'red' : 'transparent'
             }}
           />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
--- BuscadorPeliculas/src/hooks/useMovies.js
+++ BuscadorPeliculas/src/hooks/useMovies.js
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/movies.js'
 
-export function useMovies ({ search }) {
+export function useMovies ({ search, sort }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef(search) // mantiene persistencia a pesar del renderizado del componente
 
-  async function getMovies () {
+  const getMovies = useCallback(async (search) => { // callback usa memo para simplificar la sintaxis al usar funciones
+    if (search === previousSearch.current) return // memo se usa para ejecutar codigo exclusivamente al cambiar las dependencias
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
@@ -17,7 +20,13 @@ export function useMovies ({ search }) {
     } finally { // tanto en el try como en el catch
       setLoading(false)
     }
-  }
+  }, [])
 
-  return { movies, getMovies, loading }
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+  return { movies: sortedMovies, getMovies, loading }
 }
