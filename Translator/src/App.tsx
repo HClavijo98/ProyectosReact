98668f996377ebfe531965e744d588883d15d67c
zukolinoProPro <22hclavijo@ibadia.cat>
2024-12-10 17:56:49 +0100

    Translator(parte3)

--- Translator/src/App.tsx
+++ Translator/src/App.tsx
@@ -1,28 +1,58 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form, Stack } from 'react-bootstrap'
 import './App.css'
 import { useStore } from './hooks/useStore'
 import { AUTO_LANGUAGE } from './constants'
 import { ArrowsIcon } from './components/Icons'
 import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
+import { TextArea } from './components/TextArea'
 
 function App() {
-  const { fromLanguage, toLanguage, setFromLanguage, setToLanguage, interchangeLanguages } = useStore()
+  const { loading,
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    setFromLanguage,
+    setToLanguage,
+    interchangeLanguages,
+    setFromText,
+    setResult } = useStore()
   return (
     <Container fluid>
       <h1>Google Translate</h1>
 
       <Row>
         <Col>
-          <LanguageSelector onChange={setFromLanguage} />
-          {fromLanguage}
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage} />
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Stack>
         </Col>
-        <Col>
+        <Col xs='auto'>
           <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}><ArrowsIcon /></Button>
         </Col>
         <Col>
-          <LanguageSelector onChange={setToLanguage} />
-          {toLanguage}
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage} />
+            <TextArea
+            type={SectionType.To}
+            value={result}
+            onChange={setResult}
+            loading={loading}
+            />
+          </Stack>
         </Col>
       </Row>
     </Container>
---
+++ Translator/src/services/translate.ts
@@ -0,0 +1,6 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+//NO PUBLICAR ESTO O SE COLARA LA APIKEY AL CLIENTE
+//NECESARIO PASAR ESTO A BACKEND
+
+const apiKey = import.meta.env.VITE_OPENAIA_API_KEY;
