d1a9af7c83c22df789f93bf5dcd6064d5b5f40bc
zukolinoProPro <22hclavijo@ibadia.cat>
2025-01-13 18:03:22 +0100

    CrudReact+Redux(parte2)

--- CrudReactRedux/src/hooks/useUserActions.ts
+++ CrudReactRedux/src/hooks/useUserActions.ts
@@ -1,12 +1,17 @@
 import { useAppDispatch } from "./store";
-import { deleteUserById, UserId } from "../store/users/slice";
+import { addNewUser, deleteUserById, UserId } from "../store/users/slice";
 
 export function useUserActions() {
   const dispatch = useAppDispatch();
+
+  function addUser({ name, email, github }: { name: string; email: string; github: string }) {
+    dispatch(addNewUser({ name, email, github }))
+  }
+  
 
   function removeUser(id: UserId) {
     dispatch(deleteUserById(id));
   }
 
-  return { removeUser };
+  return { addUser, removeUser };
 }
